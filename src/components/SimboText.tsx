8ce55760c26729547325600b1ba5954f72591118
SimBo <simon.borho@waldmann.com>
2024-05-06 14:49:53 +0200

    SimboText

---
+++ src/components/SimboText.tsx
@@ -0,0 +1,30 @@
+import { Text as RNText, View, type ViewStyle } from 'react-native';
+import React from 'react';
+
+// styling take a look? https://dev.to/vpicone/how-to-ship-static-assets-with-your-react-native-library-22pf
+
+type SimboTextProps = {
+  content: string;
+  textAlign?: 'left' | 'center' | 'right';
+  textDecoration?: 'underline' | 'line-through';
+  viewStyle?: ViewStyle;
+};
+
+export default class SimboText extends React.Component<SimboTextProps> {
+  render() {
+    const { content, textAlign, textDecoration, viewStyle } = this.props;
+
+    return (
+      <View style={viewStyle}>
+        <RNText
+          style={{
+            textAlign: textAlign,
+            textDecorationLine: textDecoration,
+          }}
+        >
+          {content}
+        </RNText>
+      </View>
+    );
+  }
+}
