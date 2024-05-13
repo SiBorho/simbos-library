5d11c6e12ed8f3cc9d40b6e36f5c8ef2c14dcca2
SimBo <simon.borho@waldmann.com>
2024-05-13 11:01:10 +0200

    v0.1.7

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+const moduleAlias = require('module-alias');
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
 
+moduleAlias.addAliases('@simbos-style', __dirname + './src/style/index');
 export { default as SimboText } from './components/text/SimboText';
-export { SimboColor, SimboDimension } from './style/index';
