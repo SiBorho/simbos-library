65da66e0d77808b37b03622f941a6bedaf46c207
SimBo <simon.borho@waldmann.com>
2024-05-13 11:15:53 +0200

    v0.1.20

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,5 @@ export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
 
-moduleAlias.addAliases('@simbos-style', __dirname + './src/style/index');
+moduleAlias.addAliases('@simbos-style', '.lib/typescript/src/index.tsx');
 export { default as SimboText } from './components/text/SimboText';
